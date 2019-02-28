9a980b5ffb6ef501fe49bc9076d316c07fcd371b
zshi2 <zshi2@student.unimelb.edu.au>
2019-02-28 14:58:40 +1100

    Router done

---
+++ src/Home/Home.jsx
@@ -0,0 +1,13 @@
+import React, {Component} from 'react';
+import {MDBContainer, MDBRow, MDBCol, MDBView} from 'mdbreact';
+
+class Home extends React.Component {
+    render() {
+        return (
+            <div id="Home">
+            </div>
+        )
+    }
+}
+
+export default Home;
